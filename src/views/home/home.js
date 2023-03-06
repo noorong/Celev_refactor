be6e699338463543fd54d89a294205483106c932
noorong <100012522+noorong@users.noreply.github.com>
2023-03-07 01:14:52 +0900

    chore: 번들링, 의존성 분석중

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -111,17 +111,4 @@ async function insertProductElement() {
       }
     }
   });
-
-  const logoutBtn1 = document.querySelector("#logout1");
-  const logoutBtn2 = document.querySelector("#logout2");
-
-  logoutBtn1.addEventListener("click", () => {
-    sessionStorage.removeItem("token");
-    window.location.href = "/";
-  });
-
-  logoutBtn2.addEventListener("click", () => {
-    sessionStorage.removeItem("token");
-    window.location.href = "/";
-  });
 }
